2aa5f3e4bcafc552a1ff7b1e6b0d6bda1c9071f3
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-04-23 17:45:05 -0500

    rendering data

--- python/django/mysite/food/views.py
+++ python/django/mysite/food/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world")
+    item_list = Item.objects.all()
+    return HttpResponse(item_list)
 
 def item(request):
     return HttpResponse("<h1>This is a item view</h1>")
