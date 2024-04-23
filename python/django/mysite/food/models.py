fbf6281e2a5d6ec79d3c4f1469d7a0f0d376f9f3
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-04-23 17:32:04 -0500

    django admin panel

--- python/django/mysite/food/models.py
+++ python/django/mysite/food/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
+    
+    def __str__(self):
+        return self.item_name
+    
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
