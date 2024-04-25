658d77319f61bdc2cdb4e641cd567c60ba399a66
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-04-25 17:10:31 -0500

    namespacing

--- python/django/mysite/food/urls.py
+++ python/django/mysite/food/urls.py
@@ -1,6 +1,7 @@
 from . import views
 from django.urls import path
 
+app_name = 'food'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:item_id>/', views.detail, name='detail'),
