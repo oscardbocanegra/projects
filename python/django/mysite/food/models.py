4d0ef19e2cf1c711d8d846791e69c2661ce2c3f8
Oscar David Bocanegra Capera <davidbocanegrac@gmail.com>
2024-05-08 14:12:07 -0500

    adding image

--- python/django/mysite/food/models.py
+++ python/django/mysite/food/models.py
@@ -9,3 +9,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://www.miagrillandbar.com/wp-content/uploads/2020/04/dish-image-no.jpg")
